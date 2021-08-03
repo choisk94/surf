fdcac4f2220928ec50669f790e30388b842f5d61
LEEYEJI501 <yeaji0024@gmail.com>
2021-08-03 17:35:53 +0900

    feat : 펀딩 클래스 목록 조회 기능 추가
    author : yeji lee
    
    펀딩 클래스 목록 조회 기능 추가
    - add surf.lecture.controller.LectureController
    - add surf.lecture.model.dao.LectureDao
    - add surf.lecture.model.vo.Lecture
    - add resources.mappers.lecture-mapper.xml

--- surf/src/main/java/com/kh/surf/lecture/controller/LectureController.java
+++ surf/src/main/java/com/kh/surf/lecture/controller/LectureController.java
@@ -1,16 +1,41 @@
 package com.kh.surf.lecture.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.kh.surf.common.model.vo.PageInfo;
+import com.kh.surf.common.template.Pagination;
 import com.kh.surf.lecture.model.service.LectureService;
+import com.kh.surf.lecture.model.vo.Lecture;
 
 @Controller
 public class LectureController {
 	
 	@Autowired
 	private LectureService lService;
+	
+	/**
+	 * @author leeyeji
+	 * 펀딩 클래스 목록 조회
+	 */
+	@RequestMapping("funding.lec")
+	public ModelAndView selectFundingList(ModelAndView mv, @RequestParam(value="currentPage", defaultValue="1") int currentPage) {
+		
+		int listCount = lService.selectFundingCount();
+		
+		PageInfo pi = Pagination.getPageInfo(listCount, currentPage, 5, 12);
+		ArrayList<Lecture> fList = lService.selectFundingList(pi);
+		
+		mv.addObject("pi", pi)
+		  .addObject("fList", fList)
+		  .setViewName("lecture/fundingLectureList");
+		return mv;
+		
+	}
 	
 }
